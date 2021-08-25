11a25a4e2cb091118833a72313eaf3006c5722b1
John-teology <teologowilhlem@gmail.com>
2021-08-26 01:55:19 +0800

    leapyear

--- leap_year.py
+++ leap_year.py
@@ -1,4 +1,9 @@
 def is_leap(year):
+    '''
+    function takes 1 arg which a year an integer type
+    
+    and it tells whether its a leap year or not
+    '''
     leap =  True if year%4 == 0 and (year%100 != 0 or year%400 == 0) else False
     return leap
 
